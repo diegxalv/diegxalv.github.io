87b8ca7c5e64a7c845a177e6df634855ca1d09b5
Juan Diego Mesa <juaandii13@gmail.com>
2021-11-04 10:47:45 +0000

    Se añade la página

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+/* ----------------------------- 
+Preloader
+----------------------------- */
+$(window).load(function() {
+	'use strict';
+	$('.preloader').delay(600).fadeOut();
+	$('loading-webpage').delay(1000).fadeOut('slow');
+});
+
+/* About me section tab-nav function */
+$(window).ready(function() {
+	'use strict';
+
+	$('.tab-nav li').on('click', function(e) {
+		var this_li = $(this),
+			tabs = this_li.parent().parent(),
+			target = $.trim(this_li.find('a').attr('href').substring(1)),
+			all_tab_li = tabs.find('ul li');
+			
+			all_tab_li.removeClass('tab-selected').find('a[href="#'+ target +'"]')
+			.parent().addClass('tab-selected');
+			
+			tabs.find('.tab').fadeIn();
+			tabs.find('.tab:not(".'+ target +'")').hide();
+			e.preventDefault();
+			
+	});
+
+
+	$('.navigation').onePageNav({
+		currentClass: 'current',
+		scrollSpeed: 1000
+	});
+});
+
+/* FADEIN Y TOGGLE BOTON DE CONTACTO MUESTRA REDES SOCIALES*/
+
+$(document).ready(function(){
+  $('#btn-networks').click(function(){
+    $('#social-networks').fadeToggle();
+  });
+});
